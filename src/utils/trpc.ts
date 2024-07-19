fc411173597878d334455d148a952b56f83acc4a
Alberto R. Noguez <betodevq@gmail.com>
2024-07-19 09:33:37 -0300

    deleted base url usage in trpc

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -2,24 +2,12 @@ import { createTRPCReact } from "@trpc/react-query";
 import { httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "@/server/routers/_app";
 
-function getBaseUrl() {
-  if (typeof window !== "undefined") {
-    return "";
-  }
-
-  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-  }
-
-  return `http://localhost:${process.env.PORT ?? 3000}`;
-}
-
 export const trpc = createTRPCReact<AppRouter>();
 
 export const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
-      url: `${getBaseUrl()}/api/trpc`,
+      url: `/api/trpc`,
     }),
   ],
 });
