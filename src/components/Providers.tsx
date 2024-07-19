43b351d74836ae4db89aed4f5358e1357991ef3a
Alberto R. Noguez <betodevq@gmail.com>
2024-07-19 10:04:28 -0300

    deleted unnecessary trpc client duplicate

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,28 +1,12 @@
 "use client";
-// Libraries
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState, ReactNode } from "react";
-
-// TRPC
-import { httpBatchLink } from "@trpc/client";
-
-// Contexts
 import { LanguageProvider } from "@/contexts/LanguageContext";
-
-// Utils
-import { trpc } from "@/utils/trpc";
+import { trpc, trpcClient } from "@/utils/trpc";
 
 export default function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({}));
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
-        }),
-      ],
-    })
-  );
+
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -4,10 +4,22 @@ import type { AppRouter } from "@/server/routers/_app";
 
 export const trpc = createTRPCReact<AppRouter>();
 
+function getBaseUrl() {
+  if (typeof window !== "undefined") {
+    return "";
+  }
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 export const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
-      url: `/api/trpc`,
+      url: `${getBaseUrl()}/api/trpc`,
     }),
   ],
 });
