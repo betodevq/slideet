3b5891d7b8a7228b6d0e698e3e749a12748b4fb1
Alberto R. Noguez <betodevq@gmail.com>
2024-07-19 01:41:18 -0300

    Added vercel URL

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -8,7 +8,7 @@ function getBaseUrl() {
   }
 
   if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   }
 
   return `http://localhost:${process.env.PORT ?? 3000}`;
